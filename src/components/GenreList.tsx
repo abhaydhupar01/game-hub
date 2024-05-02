183a9d4fbb5903f961454a352117675908bc5679
abhay-dhupar <itsmedhupar@gmail.com>
2024-05-02 14:23:53 +0530

    Fetching genres using react query

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -25,7 +25,7 @@ const GenreList = ({ selectedGenre, onSelectedGenre }: Props) => {
         Genre
       </Heading>
       <List>
-        {data?.map((genre) => (
+        {data?.results.map((genre) => (
           <ListItem key={genre.id} marginY={1}>
             <HStack>
               <Image
