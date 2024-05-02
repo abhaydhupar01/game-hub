a4c912cabe0773faa74dcc14f3262edc8fb25bc4
abhay-dhupar <itsmedhupar@gmail.com>
2024-05-02 16:17:09 +0530

    Fetching games using React Query

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,11 +1,9 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames, { Platform } from "../hooks/useGames";
+import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-import { Genre } from "../hooks/useGenres";
 import { GameQuery } from "../App";
-
 interface Props {
   gameQuery: GameQuery;
 }
@@ -13,7 +11,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { error, data, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
 
-  if (error) return <Text>{error}</Text>;
+  if (error) return <Text>{error.message}</Text>;
   return (
     <SimpleGrid
       columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
@@ -26,7 +24,7 @@ const GameGrid = ({ gameQuery }: Props) => {
             <GameCardSkeleton />{" "}
           </GameCardContainer>
         ))}
-      {data.map((data) => (
+      {data?.results.map((data) => (
         <GameCardContainer key={data.id}>
           <GameCard game={data} />
         </GameCardContainer>
