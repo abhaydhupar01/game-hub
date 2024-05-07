2e5b43df26362c8ad2e74848ed59cb27884ebced
abhay-dhupar <itsmedhupar@gmail.com>
2024-05-07 14:37:15 +0530

    Moved entity interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,22 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
 import APIClient from "../services/api-client";
+import { Genre } from "../entities/Genre";
 
-const apiClient = new APIClient<Genre>('/genres');
+const apiClient = new APIClient<Genre>("/genres");
 
-export interface Genre{
-    id: number;
-    name: string;
-    image_background: string;
-}
-
-
-const useGenres = () => useQuery({
-    queryKey: ['genres'],
+const useGenres = () =>
+  useQuery({
+    queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24*60*60*1000, // 24 hours
-    initialData: genres
-})
-
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    initialData: genres,
+  });
 
 export default useGenres;
