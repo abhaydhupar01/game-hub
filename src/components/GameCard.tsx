576165de4106c93f20d10b9d7a5ba9bc9c030269
abhay-dhupar <itsmedhupar@gmail.com>
2024-05-08 07:59:35 +0530

    Refactor updated game detail page link

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,20 +10,24 @@ interface Props {
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <Card marginTop={3} marginStart={2} borderRadius={10}>
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <HStack justifyContent={"space-between"} marginBottom={2}>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic}></CriticScore>
-        </HStack>
-        <Heading fontSize="xl" marginBottom={2}>
-          <Link to={"/games/" + game.slug}>{game.name}</Link>
-        </Heading>
-      </CardBody>
-    </Card>
+    <>
+      <Link to={"/games/" + game.slug}>
+        <Card marginTop={3} marginStart={2} borderRadius={10}>
+          <Image src={getCroppedImageUrl(game.background_image)} />
+          <CardBody>
+            <HStack justifyContent={"space-between"} marginBottom={2}>
+              <PlatformIconList
+                platforms={game.parent_platforms.map((p) => p.platform)}
+              />
+              <CriticScore score={game.metacritic}></CriticScore>
+            </HStack>
+            <Heading fontSize="xl" marginBottom={2}>
+              {game.name}
+            </Heading>
+          </CardBody>
+        </Card>
+      </Link>
+    </>
   );
 };
 
